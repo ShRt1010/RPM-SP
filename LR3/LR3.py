c83e5a1317d2ed65ff88d68e67c4405b227f2e55
ShRt1010 <ratmir.sharapov.2007@gmail.com>
2026-05-07 05:00:18 +0500

    Update LR3.py

--- LR3/LR3.py
+++ LR3/LR3.py
@@ -1,37 +1,46 @@
 import pandas as pd
 
-file_name = "lab3.xlsx"
+# Создаю таблицу со студентами
+students = pd.DataFrame({
+    "Имя": ["Иван", "Петр", "Анна"],
+    "Возраст": [18, 19, 18],
+    "Оценка": [5, 4, 5]
+})
 
-# Создаем Excel-файл с несколькими листами
-with pd.ExcelWriter(file_name) as writer:
-    pd.DataFrame({
-        "Имя": ["Иван", "Анна", "Олег"],
-        "Возраст": [19, 20, 18],
-        "Группа": ["ИСП-21", "ИСП-21", "ИСП-22"]
-    }).to_excel(writer, sheet_name="Студенты", index=False)
+# Создаю таблицу с товарами
+products = pd.DataFrame({
+    "Товар": ["Хлеб", "Молоко", "Сыр"],
+    "Цена": [45, 80, 250],
+    "Количество": [2, 1, 1]
+})
 
-    pd.DataFrame({
-        "Товар": ["Мышь", "Клавиатура", "Монитор"],
-        "Цена": [1200, 2500, 15000],
-        "Количество": [5, 3, 2]
-    }).to_excel(writer, sheet_name="Товары", index=False)
+# Создаю Excel-файл с двумя листами
+with pd.ExcelWriter("data.xlsx") as writer:
+    students.to_excel(writer, sheet_name="Студенты", index=False)
+    products.to_excel(writer, sheet_name="Товары", index=False)
 
-    pd.DataFrame({
-        "Предмет": ["Python", "C#", "Базы данных"],
-        "Оценка": [5, 4, 5]
-    }).to_excel(writer, sheet_name="Оценки", index=False)
+print("Файл Excel создан")
 
-print("Первый лист:")
-print(pd.read_excel(file_name))
 
-print("\nЛист Товары, только 2 столбца и 2 строки:")
-print(pd.read_excel(file_name, sheet_name="Товары", usecols=["Товар", "Цена"], nrows=2))
+# Вывожу весь лист Студенты
+df1 = pd.read_excel("data.xlsx", sheet_name="Студенты")
+print("\nЛист Студенты:")
+print(df1)
 
-print("\nЛист Студенты с указанием типа данных:")
-print(pd.read_excel(file_name, sheet_name="Студенты", dtype={"Возраст": "string"}))
 
-print("\nСразу несколько листов:")
-sheets = pd.read_excel(file_name, sheet_name=["Студенты", "Оценки"])
-for name, table in sheets.items():
-    print(f"\n{name}")
-    print(table)
+# Вывожу только некоторые столбцы
+df2 = pd.read_excel("data.xlsx", sheet_name="Студенты", usecols=["Имя", "Оценка"])
+print("\nТолько имя и оценка:")
+print(df2)
+
+
+# Вывожу только первые 2 строки
+df3 = pd.read_excel("data.xlsx", sheet_name="Товары", nrows=2)
+print("\nПервые две строки листа Товары:")
+print(df3)
+
+
+# Читаю сразу несколько листов
+all_sheets = pd.read_excel("data.xlsx", sheet_name=["Студенты", "Товары"])
+print("\nПрочитанные листы:")
+print(all_sheets.keys())
